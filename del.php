1d290cfe81f08a2f9cdad6495d6dec5724a28316
KitosFV <kitos200387@gmail.com>
2018-07-12 18:16:53 +0300

    upd

---
+++ del.php
@@ -0,0 +1,8 @@
+<?php
+$id = htmlspecialchars($_GET["id"]);
+$user = "pysm1t";
+$host = "db4free.net/";
+$passwd = "935afe2c";
+$name = "guestbook";
+$c = mysqli_connect($host, $user, $passwd, $name);
+?>
