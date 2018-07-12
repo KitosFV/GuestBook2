2abfffb8b268123a750946ba10797332a58e11b4
KitosFV <kitos200387@gmail.com>
2018-07-12 18:18:49 +0300

    new

--- del.php
+++ del.php
@@ -5,4 +5,8 @@ $host = "db4free.net/";
 $passwd = "935afe2c";
 $name = "guestbook";
 $c = mysqli_connect($host, $user, $passwd, $name);
+
+$query = "DELETE FROM messages WHERE id = $id";
+
+mysqli_query($c,$query);
 ?>
